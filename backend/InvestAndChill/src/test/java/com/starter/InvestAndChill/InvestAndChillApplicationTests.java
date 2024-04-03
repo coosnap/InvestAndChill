8f42844b235f3dedb48758640d082cd77d4d9640
ThanhLam <lamphongthanh1992@gmail.com>
2024-04-03 11:30:14 +0700

    update project

--- backend/InvestAndChill/src/test/java/com/starter/InvestAndChill/InvestAndChillApplicationTests.java
+++ backend/InvestAndChill/src/test/java/com/starter/InvestAndChill/InvestAndChillApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class InvestAndChillApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	/*
+	 * @Test void contextLoads() { }
+	 */
 
 }
