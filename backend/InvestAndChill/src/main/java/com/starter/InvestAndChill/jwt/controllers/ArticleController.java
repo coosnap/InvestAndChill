47f1f68e8065563036d92c7227615399c4403a1c
ThanhLam <lamphongthanh1992@gmail.com>
2024-05-06 16:49:25 +0700

    update delete pathc

--- backend/InvestAndChill/src/main/java/com/starter/InvestAndChill/jwt/controllers/ArticleController.java
+++ backend/InvestAndChill/src/main/java/com/starter/InvestAndChill/jwt/controllers/ArticleController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -78,6 +79,16 @@ public class ArticleController {
 	      return new ResponseEntity<>(articleRepository.save(_article), HttpStatus.OK);
 	    } else {
 	      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	    }
+	  }
+	 
+	 @DeleteMapping("/{id}")
+	  public ResponseEntity<HttpStatus> deleteArticle(@PathVariable("id") int id) {
+	    try {
+	    	articleRepository.deleteById(id);
+	      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	    } catch (Exception e) {
+	      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	    }
 	  }
 	 
