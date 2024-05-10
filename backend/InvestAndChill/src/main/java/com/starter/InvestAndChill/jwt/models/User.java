964136f0c788ffaa1d7497758c0b47867291b525
ThanhLam <lamphongthanh1992@gmail.com>
2024-05-10 11:09:17 +0700

    test

--- backend/InvestAndChill/src/main/java/com/starter/InvestAndChill/jwt/models/User.java
+++ backend/InvestAndChill/src/main/java/com/starter/InvestAndChill/jwt/models/User.java
@@ -71,7 +71,7 @@ public class User {
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
-
+	//add comment
 	public Date getDateOfBirth() {
 		return dateOfBirth;
 	}
