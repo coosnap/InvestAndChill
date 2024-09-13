67fd5a46dbc8b0f023676ba7270e59fd453c316d
Thanh Nguyen <ncthanhmh@gmail.com>
2024-09-13 18:36:59 +0700

    up

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
   },
   // server: {
   //   proxy: {
   //     '/api': 'https://agile-bayou-65029-c59bb8376f70.herokuapp.com'
   //   }
   // }
-})
+});
