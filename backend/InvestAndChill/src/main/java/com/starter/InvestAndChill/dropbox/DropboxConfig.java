62d0fb2cc00cd593d0a1a976dec808be89c8aa4d
ThanhLam <lamphongthanh1992@gmail.com>
2024-10-01 21:43:13 +0700

    test

--- backend/InvestAndChill/src/main/java/com/starter/InvestAndChill/dropbox/DropboxConfig.java
+++ backend/InvestAndChill/src/main/java/com/starter/InvestAndChill/dropbox/DropboxConfig.java
@@ -2,18 +2,21 @@ package com.starter.InvestAndChill.dropbox;
 
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DropboxConfig {
 
-    private static final String ACCESS_TOKEN = "sl.B991sshCVejWOu246OdICITZHpjw3HPD1lsrtV3tM7ONS2vw7afof3lZeZ7rgzHWo7XTapM3UPN0f5ibNrrnSSA1r3MQ4PnfU0JtaEqbQ10xJlgZrsaXXFUm3qBDr1PVrRheaLbYQR5sG-0";
+	 @Value("${dropbox.access.token}")
+	 private String accessToken;
 
     @Bean
     public DbxClientV2 dropboxClient() {
         DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/spring-boot-app").build();
-        return new DbxClientV2(config, ACCESS_TOKEN);
+        return new DbxClientV2(config, accessToken);
     }
 }
 
