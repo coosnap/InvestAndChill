e5088ddbeb1ebb8f4116dc39dc3c9d0664f8d361
ThanhLam <lamphongthanh1992@gmail.com>
2024-10-25 11:45:29 +0700

    commit

--- backend/InvestAndChill/src/main/java/com/starter/InvestAndChill/jwt/models/User.java
+++ backend/InvestAndChill/src/main/java/com/starter/InvestAndChill/jwt/models/User.java
@@ -23,7 +23,6 @@ import javax.validation.constraints.Size;
 @Table(	name = "users", 
 		uniqueConstraints = { 
 			@UniqueConstraint(columnNames = "username")
-		//	@UniqueConstraint(columnNames = "email") 
 		})
 public class User {
 	@Id
