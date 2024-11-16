939a71fd7d717a80f313745bf41d347e9d4b628b
Son Hoang Nguyen <nguyen.son240@gmail.com>
2024-11-16 20:27:44 +0700

    optimize extract mapping script

--- backend/InvestAndChill/src/main/resources/scripts/upload_mapping_script.py
+++ backend/InvestAndChill/src/main/resources/scripts/upload_mapping_script.py
@@ -1,30 +1,29 @@
-import os
-import re
-from typing import Tuple
-
 import pandas as pd
 
-from sqlalchemy import inspect, create_engine, text
+from sqlalchemy import create_engine
 db_config = {
-        'username': 'myuser',
-        'password': 'secret',
-        'host': 'localhost',
-        'port': '5432',
-        'database': 'mydatabase'
+    'username': 'myuser',
+    'password': 'secret',
+    'host': 'localhost',
+    'port': '5432',
+    'database': 'mydatabase'
 }
+# db_config = {
+#     'username': 'u5njifacefuq5b',
+#     'password': 'pe18e1299ede0f0c814d0ffb4a4a90812a968e581aef7a696f40c9969578b6606',
+#     'host': 'ca8a5csd8gqht9.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+#     'port': '5432',
+#     'database': 'd1re3fkrj0ginj'
+# }
 
 connection_string = f"postgresql://{db_config['username']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
 engine = create_engine(connection_string)
 
 # file_path = 'C:\projects\chills\TEMPLATE DATA VISUAL\PHI TAI CHINH\mapping table.xlsx'
-file_path = 'c:\projects\chills\TEMPLATE DATA VISUAL\mapping table_cal_visual_5-11-2024.xlsx'
-def fiin_map():
-    table_name = 'phi_tai_chinh_fiin_map'
-    # table_name = 'ngan_hang_fiin_map'
-    # table_name = 'chung_khoan_fiin_map'
-    sheet = 'phitaichinh'
-    # sheet = 'nganhang'
-    # sheet = 'chungkhoan'
+# file_path = 'c:\projects\chills\TEMPLATE DATA VISUAL\mapping table_cal_visual_5-11-2024.xlsx'
+file_path = 'c:\projects\chills\mapping table_cal_visual_12-11-2024.xlsx'
+def fiin_map(table_name, sheet):
+    table_name = table_name + '_fiin_map'
 
     # Read specific columns from row 5 to the end
     df = pd.read_excel(file_path, sheet_name=sheet, usecols=[1, 2, 4], skiprows=3, header=None)
@@ -36,13 +35,8 @@ def fiin_map():
     print(f"Inserted {len(df)} new records.")
     print("Data has been successfully saved to the database for fiin map")
 
-def vs_map():
-    table_name = 'phi_tai_chinh_vietstock_map'
-    # table_name = 'ngan_hang_vietstock_map'
-    # table_name = 'chung_khoan_vietstock_map'
-    sheet = 'phitaichinh'
-    # sheet = 'nganhang'
-    # sheet = 'chungkhoan'
+def vs_map(table_name, sheet):
+    table_name = table_name + '_vietstock_map'
 
     # Read specific columns from row 5 to the end
     df = pd.read_excel(file_path, sheet_name=sheet, usecols=[4, 5], skiprows=3, header=None)
@@ -55,6 +49,14 @@ def vs_map():
     print("Data has been successfully saved to the database for vietstock")
 
 if __name__ == "__main__":
-    # fiin_map()
-    vs_map()
+    # table_name = 'phi_tai_chinh'
+    # sheet = 'phitaichinh'
 
+    table_name = 'ngan_hang'
+    sheet = 'nganhang'
+
+    # table_name = 'chung_khoan'
+    # sheet = 'chungkhoan'
+
+    fiin_map(table_name, sheet)
+    vs_map(table_name, sheet)
