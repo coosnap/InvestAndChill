f3484f409764b0351b866eb62bff3636e6a6d97f
ThanhLam <lamphongthanh1992@gmail.com>
2024-11-24 19:04:51 +0700

    update phase 2

---
+++ backend/InvestAndChill/src/main/java/com/starter/InvestAndChill/jwt/payload/response/Perf4Response.java
@@ -0,0 +1,40 @@
+package com.starter.InvestAndChill.jwt.payload.response;
+
+public class Perf4Response extends ReportReponse{
+	private Double EBITTrailing;
+	private Double NetFinanceialTrailing;
+	private Double NetFinancialAdjustTrailing;
+	private Double NImgTrailing;
+	private Double EBITmTrailing;
+	public Double getEBITmTrailing() {
+		return EBITmTrailing;
+	}
+	public void setEBITmTrailing(Double eBITmTrailing) {
+		EBITmTrailing = eBITmTrailing;
+	}
+	public Double getEBITTrailing() {
+		return EBITTrailing;
+	}
+	public void setEBITTrailing(Double eBITTrailing) {
+		EBITTrailing = eBITTrailing;
+	}
+	public Double getNetFinanceialTrailing() {
+		return NetFinanceialTrailing;
+	}
+	public void setNetFinanceialTrailing(Double netFinanceialTrailing) {
+		NetFinanceialTrailing = netFinanceialTrailing;
+	}
+	public Double getNetFinancialAdjustTrailing() {
+		return NetFinancialAdjustTrailing;
+	}
+	public void setNetFinancialAdjustTrailing(Double netFinancialAdjustTrailing) {
+		NetFinancialAdjustTrailing = netFinancialAdjustTrailing;
+	}
+	public Double getNImgTrailing() {
+		return NImgTrailing;
+	}
+	public void setNImgTrailing(Double nImgTrailing) {
+		NImgTrailing = nImgTrailing;
+	}
+
+}
