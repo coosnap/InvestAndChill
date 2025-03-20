9137a32a9f06d5a50076526139a36d4d45a31da7
Thanh Nguyen <ncthanhmh@gmail.com>
2025-03-20 11:31:58 +0700

    up

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,6 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '127.0.0.1',
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
